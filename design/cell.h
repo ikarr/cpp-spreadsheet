60e592da990b78e39e4e95b84ddd49e0ccef220b
Igor <ikarrus94@gmail.com>
2023-08-07 23:23:27 +0500

    design review

--- design/cell.h
+++ design/cell.h
@@ -13,11 +13,81 @@ public:
 
     Value GetValue() const override;
     std::string GetText() const override;
+	
+	std::vector<Position> GetReferencedCells() const override;
+    const std::vector<Position>& GetStoredReferencedCells() const;
+
+    void ClearCache();
+    bool HasCache() const;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+	class Impl {
+    public:
+        virtual Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+		virtual std::vector<Position> GetReferencedCells() const {
+            return {};
+        }
+        
+        virtual ~Impl() = default;
+    };
+    
+    class EmptyImpl : public Impl {
+    public:
+        CellInterface::Value GetValue() const override {
+            return 0.0;
+        }
+		
+        std::string GetText() const override {
+            return {};
+        }
+    };
+    
+    class TextImpl : public Impl {
+    public:
+        explicit TextImpl(std::string text)
+			: text_{std::move(text)} {}
+		
+        CellInterface::Value GetValue() const override {
+            return text_.length() > 0 && (text_[0] == '\'' ? text_.substr(1) : text_);
+        }
+		
+        std::string GetText() const override {
+            return text_;
+        }
+		
+    private:
+        std::string text_;
+    };
+    
+    class FormulaImpl : public Impl {
+    public:
+        FormulaImpl(std::string text, SheetInterface& sheet)
+			: formula_{ParseFormula(std::move(text))}, sheet_{sheet} {}
+			
+        CellInterface::Value GetValue() const override {
+            FormulaInterface::Value val = formula_->Evaluate(sheet_);
+            if (std::holds_alternative<double>(val)) {
+                return std::get<double>(val);
+            }
+            return std::get<FormulaError>(val);
+        }
+		
+        std::string GetText() const override {
+            return '=' + formula_->GetExpression();
+        }
+
+        std::vector<Position> GetReferencedCells() const override {
+            return formula_->GetReferencedCells();
+        }
+
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+        const SheetInterface& sheet_;
+    };
+    
     std::unique_ptr<Impl> impl_;
+	SheetInterface& sheet_;
+    std::vector<Position> cell_refs_;
+    mutable std::unique_ptr<Value> cache_;
 };
--- design/formula.h
+++ design/formula.h
@@ -6,24 +6,23 @@
 
 #include <memory>
 #include <variant>
+#include <vector>
 #include <sstream>
 
-// Формула, позволяющая вычислять и обновлять арифметическое выражение.
-// Поддерживаемые возможности:
-// * Простые бинарные операции и числа, скобки: 1+2*3, 2.5*(2+3.5/7)
+// Формула, позволяющая вычислять и обновлять арифметическое выражение
 class FormulaInterface {
 public:
     using Value = std::variant<double, FormulaError>;
 
     virtual ~FormulaInterface() = default;
 
-    // Возвращает вычисленное значение формулы либо ошибку. На данном этапе
-    // мы создали только 1 вид ошибки -- деление на 0.
-    virtual Value Evaluate() const = 0;
+    virtual Value Evaluate(const SheetInterface& sheet) const = 0;
 
-    // Возвращает выражение, которое описывает формулу.
-    // Не содержит пробелов и лишних скобок.
     virtual std::string GetExpression() const = 0;
+	
+	// Возвращает список ячеек, задействованных в вычислении формулы.
+	// Список отсортирован по возрастанию и не содержит повторяющихся ячеек
+    virtual std::vector<Position> GetReferencedCells() const = 0;
 };
 
 // Парсит переданное выражение и возвращает объект формулы.
